7e26045ed050fea0ce5c8e92b8c94d1d4d712f69
NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2019-02-07 09:58:46 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# workshop
+
+# Workshop4
+
+## Part I
+1. 
+> What is the reason of this CPU consumption? <br>
+> **Answer:** the reason is because there is an active wait in the producer. <br>
+
+ ![jVisualVM image](image/CPUconsumption.png)
+
+> What is the class responsible of that consumption? <br>
+> **Answer:** The class responsible for the CPU consume is Producer.
+
+2. 
+![jVisualVM image](image/decreased.png)
+
+3. 
+![jVisualVM image](image/LowConsumer.png)
+
+## Part II
---
+++ TheSuspension/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Consumer extends Thread{
+    
+    private Queue<Integer> queue;
+    
+    
+    public Consumer(Queue<Integer> queue){
+        this.queue=queue;        
+    }
+    
+    @Override
+    public void run() {
+        
+        while (true) {
+
+            if (queue.size() > 0) {
+                int elem=queue.poll();
+                System.out.println("Consumer consumes "+elem);                                
+            }
+            
+            if (queue.isEmpty()){
+                try {
+                    queue.wait();
+                    Thread.sleep(200);                    
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                queue.notifyAll();
+                
+            }
+
+        }
+    }
+}
